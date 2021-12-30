edb9e6c1977d57d59d13777fa80d66e398af72df
Yonohi <loic.planat2@gmail.com>
2021-12-30 12:33:07 +0100

    actualisation

--- server.py
+++ server.py
@@ -19,7 +19,6 @@ app.secret_key = 'something_special'
 
 competitions = loadCompetitions()
 clubs = loadClubs()
-initial_clubs = loadClubs()
 
 @app.route('/')
 def index():
@@ -51,18 +50,22 @@ def book(competition,club):
 @app.route('/purchasePlaces',methods=['POST'])
 def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    initial_club = [c for c in initial_clubs if c['name'] == request.form['club']][0]
-    initial_club_points = int(initial_club['points'])
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
     # test places correctes pour le nombre de points du clubs
     if 0 < placesRequired <= int(club['points']):
+        try :
+            nb_points = int(competition[f"{club['name']}"])
+        except KeyError:
+            competition[f"{club['name']}"] = 0
+            nb_points = int(competition[f"{club['name']}"])
         if 0 < placesRequired <= 12 and\
-                0 < placesRequired <= (12 - (initial_club_points-int(club['points']))):
+                0 < placesRequired <= (12 - nb_points):
             # test nombre ne dépassant pas le nombre de places dispo
             if placesRequired <= int(competition['numberOfPlaces']):
                 competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
                 club['points'] = int(club['points']) - placesRequired
+                competition[f"{club['name']}"] += placesRequired
                 flash('Great-booking complete!')
                 return render_template('welcome.html', club=club,
                                        competitions=competitions)
