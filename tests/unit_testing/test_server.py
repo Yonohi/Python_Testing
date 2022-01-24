bfb966471d791afb668e704c2308e04735d3cb55
Yonohi <loic.planat2@gmail.com>
2022-01-24 12:32:48 +0100

    changement nom variable

--- tests/unit_testing/test_server.py
+++ tests/unit_testing/test_server.py
@@ -60,21 +60,21 @@ def test_old_book(mocker, client, clubs, old_competitions):
 #               1) club et comp > 12
 #               2) club < 12 et comp >12
 #               3) club < 12, comp < 12 et club > comp
-@pytest.mark.parametrize("points", [-1, 0, 1, 6, 7, 8, 9, 12, 13])
-def test_purchase_places(mocker, points, client, competitions, clubs):
+@pytest.mark.parametrize("places", [-1, 0, 1, 6, 7, 8, 9, 12, 13])
+def test_purchase_places(mocker, places, client, competitions, clubs):
     mocker.patch.object(server, 'competitions', competitions)
     mocker.patch.object(server, 'clubs', clubs)
 
     # 1) club et comp > 12
-    if points in [-1, 0, 1, 7, 12, 13]:
+    if places in [-1, 0, 1, 7, 12, 13]:
         response = client.post('/purchasePlaces',
-                               data=dict(places=points,
+                               data=dict(places=places,
                                          competition='Test Comp+12',
                                          club='Test Club+12'),
                                follow_redirects=True)
 
         # test strictement positif
-        if points in [-1, 0]:
+        if places in [-1, 0]:
             message = "Test Club+12 can't use this number of points." \
                       f" Number available: {25 * POINTS_PER_PLACE}".replace("'", "&#39;")
             assert message in response.data.decode()
@@ -82,50 +82,50 @@ def test_purchase_places(mocker, points, client, competitions, clubs):
             assert int(competitions[0]['numberOfPlaces']) == 25
 
         # 13 (12+1)
-        elif points in [13]:
+        elif places in [13]:
             message = "A club can't book more than 12 places.".replace("'", "&#39;")
             assert message in response.data.decode()
             assert int(clubs[0]['points']) == 25 * POINTS_PER_PLACE
             assert int(competitions[0]['numberOfPlaces']) == 25
 
         # test nb ok
-        elif points in [1, 7, 12]:
+        elif places in [1, 7, 12]:
             message = 'Great-booking complete!'
             assert message in response.data.decode()
-            if points == 1:
+            if places == 1:
                 assert int(clubs[0]['points']) == 24 * POINTS_PER_PLACE
                 assert int(competitions[0]['numberOfPlaces']) == 24
 
     # test succession de demande > 12 (2*7 à l'aide du test précédent)
-    if points in [7]:
+    if places in [7]:
         response = client.post('/purchasePlaces',
-                               data=dict(places=points,
+                               data=dict(places=places,
                                          competition='Test Comp+12',
                                          club='Test Club+12'),
                                follow_redirects=True)
         message = "A club can't book more than 12 places.".replace("'",
                                                                    "&#39;")
         assert message in response.data.decode()
-        if points == 7:
+        if places == 7:
             assert int(clubs[0]['points']) == 18 * POINTS_PER_PLACE
             assert int(competitions[0]['numberOfPlaces']) == 18
 
     # 2) club < 12 et comp >12
-    if points in [8, 9]:
+    if places in [8, 9]:
         # remarque: le club testé à plus de points que la competition
         response = client.post('/purchasePlaces',
-                               data=dict(places=points,
+                               data=dict(places=places,
                                          competition='Test Comp+12',
                                          club='Test Club-12'),
                                follow_redirects=True)
         # test max points club
-        if points in [8]:
+        if places in [8]:
             message = "Great-booking complete!"
             assert message in response.data.decode()
             assert int(clubs[1]['points']) == 0 * POINTS_PER_PLACE
             assert int(competitions[0]['numberOfPlaces']) == 17
         # test max+1
-        elif points in [9]:
+        elif places in [9]:
             message = "Test Club-12 can't use this number of points." \
                       f" Number available: {8 * POINTS_PER_PLACE}".replace("'", "&#39;")
             assert message in response.data.decode()
@@ -133,15 +133,15 @@ def test_purchase_places(mocker, points, client, competitions, clubs):
             assert int(competitions[0]['numberOfPlaces']) == 25
 
     # 3) club < 12, comp < 12 et club > comp
-    if points in [6]:
+    if places in [6]:
         # remarque: le club testé à plus de points que la competition
         response = client.post('/purchasePlaces',
-                               data=dict(places=points,
+                               data=dict(places=places,
                                          competition='Test Comp-12',
                                          club='Test Club-12'),
                                follow_redirects=True)
         # test nb dépassant le nb de points pour la compet
-        if points in [6]:
+        if places in [6]:
             message = "The number requested is greater than the number of " \
                       "places available."
             assert message in response.data.decode()
